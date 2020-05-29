b1bf87967ca0d88c45f38cf57eda2cfa60eb5914
Robert Rollins <rrollins@caltech.edu>
2020-05-28 18:09:40 -0700

    The hierarchical Collections code is all in place.
    All that's missing now is a migration, which I'm kindof baffled on how to even create. I asked for advice in the Wagtail Slack.
    
    With that in place, I can start testing this in a raw Django project, to figure how to to make sure it works.

--- wagtail/admin/navigation.py
+++ wagtail/admin/navigation.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-from wagtail.core.models import Page
+from wagtail.core.models import Collection, Page
 
 
 def get_pages_with_direct_explore_permission(user):
@@ -13,6 +13,33 @@ def get_pages_with_direct_explore_permission(user):
             group_permissions__group__in=user.groups.all(),
             group_permissions__permission_type__in=['add', 'edit', 'publish', 'lock']
         )
+
+
+def get_collections_with_management_permission(user):
+    # Get all Collections that the User has permissions for.
+    qs = Collection.objects
+
+    if user.is_superuser:
+        # Superusers have permission on all Collections.
+        return qs.filter(depth=1)
+
+    return qs.filter(
+        group_manage_permissions__group__in=user.groups.all(),
+        group_manage_permissions__permission_type__in=['add', 'edit', 'bulk_delete']
+    ).distinct()
+
+
+def get_manageable_root_collection(user):
+    # Get the highest common manageable ancestor for the given user. If the user
+    # has no permissions over any collections, this method will return None.
+    collections = get_collections_with_management_permission(user)
+    if collections:
+        return collections.first_common_ancestor(
+            include_self=True,
+            strict=True
+        )
+    else:
+        return None
 
 
 def get_explorable_root_page(user):
